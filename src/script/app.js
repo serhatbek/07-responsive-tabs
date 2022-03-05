985cab3bbac6438bf577babc6888820819936645
serhatbek <serhat.bekk@gmail.com>
2022-03-06 01:48:10 +0300

    tabs functionality

--- src/script/app.js
+++ src/script/app.js
@@ -1,3 +1,28 @@
 if (module.hot) {
   module.hot.accept();
 }
+
+const tabBtns = document.querySelectorAll('.underwater__tabs-btn'),
+  tabContents = document.querySelectorAll('.underwater__tabs-content');
+
+tabBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    showTabPanel(e);
+  });
+});
+
+function showTabPanel(e) {
+  const tabId = e.target.dataset.tabTarget;
+  const tabTarget = document.getElementById(tabId);
+
+  tabBtns.forEach((tabBtn) => {
+    tabBtn.classList.remove('js-tab-active');
+  });
+
+  tabContents.forEach((tabContent) => {
+    tabContent.classList.remove('js-tab-active');
+  });
+
+  e.target.classList.add('js-tab-active');
+  tabTarget.classList.add('js-tab-active');
+}
